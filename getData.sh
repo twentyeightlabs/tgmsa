8f133d79469e737cd7a2ff108af9d76cc15e05d1
twentyeightlabs <twentyeightlabs@users.noreply.github.com>
2017-05-12 10:01:07 +0200

    add checking if dir exists

--- getData.sh
+++ getData.sh
@@ -9,7 +9,9 @@ for i in $(seq 393 395); do
 	#echo $i
 	#bash dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'|sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g"|tr '\n' ';'
 	cd $TGMSAHOME
-	mkdir $TGMSAHOME/$DATE
+	if [ ! -d $TGMSAHOME/$DATE ]; then
+		mkdir $TGMSAHOME/$DATE
+	fi
 	bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ',' >> $TGMSAHOME/$DATE/report-$DATE.csv
 	BOXNAME=`mysql accoDb -e "select nazwaUzytkownika from Zdarzenie where (idUzytkownik = $i)"|sort -u|grep -v nazwaUzytkownika`
 	echo $BOXNAME
