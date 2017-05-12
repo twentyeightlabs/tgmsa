0e12de8305501513f5d98c6c502f0164c68f9f33
root <root@ip-172-31-29-212.eu-central-1.compute.internal>
2017-05-12 07:55:31 +0000

    change home to thmsahome

--- getData.sh
+++ getData.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-HOME=/root/tgmsa
+TGMSAHOME=/root/tgmsa
 BOXES=395
 #DATE=`date +"%Y-%m-%d"`
 DATE="2017-04-01"
@@ -8,8 +8,9 @@ DATE="2017-04-01"
 for i in $(seq 393 395); do
 	#echo $i
 	#bash dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'|sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g"|tr '\n' ';'
-	cd $HOME
-	bash $HOME/dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ',' >> $HOME/data-$DATA.csv
+	cd $TGMSAHOME
+	mkdir $TGMSAHOME/$DATE
+	bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ',' >> $TGMSAHOME/$DATE/report-$DATE.csv
 	BOXNAME=`mysql accoDb -e "select nazwaUzytkownika from Zdarzenie where (idUzytkownik = $i)"|sort -u|grep -v nazwaUzytkownika`
 	echo $BOXNAME
 done
