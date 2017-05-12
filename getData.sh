3c5c533808744da9e702289443178ed5c74d2a7d
twentyeightlabs <twentyeightlabs@users.noreply.github.com>
2017-05-12 09:43:08 +0200

    add home dir

--- getData.sh
+++ getData.sh
@@ -1,5 +1,6 @@
 #/bin/bash
 
+HOME=/root/tgmsa
 BOXES=395
 #DATE=`date +"%Y-%m-%d"`
 DATE="2017-04-01"
@@ -7,7 +8,8 @@ DATE="2017-04-01"
 for i in $(seq 393 395); do
 	#echo $i
 	#bash dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'|sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g"|tr '\n' ';'
-	bash dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ','
+	cd $HOME
+	bash $HOME/dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ',' >> $HOME/data-$DATA.csv
 	BOXNAME=`mysql accoDb -e "select nazwaUzytkownika from Zdarzenie where (idUzytkownik = $i)"|sort -u|grep -v nazwaUzytkownika`
 	echo $BOXNAME
 done
