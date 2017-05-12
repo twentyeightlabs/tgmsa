b9c0a9283c17b24c3f87ddd8f02e51b889c795a6
twentyeightlabs <artur@twentyeightlabs.com>
2017-05-12 21:22:34 +0200

    remove /dev/null

--- getData.sh
+++ getData.sh
@@ -25,12 +25,12 @@ for i in $(seq 393 395); do
 	bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep $DATE | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ',' >> $TGMSAHOME/reports/$DATE/report-$DATE.csv
 
 # get open hours from Db
-	OPENHOUR=`bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep $DATE | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ','|cut -d "," -f1` &>/dev/null
+	OPENHOUR=`bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep $DATE | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ','|cut -d "," -f1`
 	OPENHOURSEC=`echo $OPENHOUR | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'` > /dev/null 2>&1
-	CLOSEHOUR=`bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep $DATE | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ','|cut -d "," -f2` > /dev/null 2>&1
-	CLOSEHOURSEC=`echo $CLOSEHOUR | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'` > /dev/null 2>&1
-	TIMEOPENSEC=`expr $CLOSEHOURSEC - $OPENHOURSEC` > /dev/null 2>&1
-	TIMEOPEN=`date -d @$TIMEOPENSEC +%H:%M:%S` > /dev/null 2>&1
+	CLOSEHOUR=`bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep $DATE | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ','|cut -d "," -f2`
+	CLOSEHOURSEC=`echo $CLOSEHOUR | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }'`
+	TIMEOPENSEC=`expr $CLOSEHOURSEC - $OPENHOURSEC`
+	TIMEOPEN=`date -d @$TIMEOPENSEC +%H:%M:%S`
 
 ########DEBUG MESSAGES#################
 #	echo open $OPENHOUR
