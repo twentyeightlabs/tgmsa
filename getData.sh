85d9dd899ecca11c76b904a88173ba4917d8a85f
root <root@ip-172-31-29-212.eu-central-1.compute.internal>
2017-05-12 08:03:02 +0000

    add boxname to csv

--- getData.sh
+++ getData.sh
@@ -14,7 +14,7 @@ for i in $(seq 393 395); do
 	fi
 	bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep "2017-04-01" | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ',' >> $TGMSAHOME/$DATE/report-$DATE.csv
 	BOXNAME=`mysql accoDb -e "select nazwaUzytkownika from Zdarzenie where (idUzytkownik = $i)"|sort -u|grep -v nazwaUzytkownika`
-	echo $BOXNAME
+	echo $BOXNAME >> $TGMSAHOME/$DATE/report-$DATE.csv
 done
 
 #bash dbData.sh | grep -v czas | awk 'NR==1; END{print}'|sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g"|tr '\n' ';'
