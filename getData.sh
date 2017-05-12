b48f895f8eec79c5bf4fe4836395e78211e99c25
twentyeightlabs <twentyeightlabs@users.noreply.github.com>
2017-05-12 10:23:33 +0200

    remove unnesesary comments

--- getData.sh
+++ getData.sh
@@ -6,8 +6,6 @@ BOXES=395
 DATE="2017-04-01"
 
 for i in $(seq 393 395); do
-	#echo $i
-	#bash dbData.sh $i | grep -v czas| grep $DATE | awk 'NR==1; END{print}'|sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g"|tr '\n' ';'
 	cd $TGMSAHOME
 	if [ ! -d $TGMSAHOME/$DATE ]; then
 		mkdir $TGMSAHOME/$DATE
@@ -16,5 +14,3 @@ for i in $(seq 393 395); do
 	BOXNAME=`mysql accoDb -e "select nazwaUzytkownika from Zdarzenie where (idUzytkownik = $i)"|sort -u|grep -v nazwaUzytkownika`
 	echo $BOXNAME >> $TGMSAHOME/$DATE/report-$DATE.csv
 done
-
-#bash dbData.sh | grep -v czas | awk 'NR==1; END{print}'|sed "s/'/\'/;s/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g"|tr '\n' ';'
