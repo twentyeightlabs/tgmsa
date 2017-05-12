c3df0b5c13acd1e1c211759a75cfb4fa12546724
twentyeightlabs <twentyeightlabs@users.noreply.github.com>
2017-05-12 10:35:40 +0200

    add time variables

--- getData.sh
+++ getData.sh
@@ -2,6 +2,11 @@
 
 TGMSAHOME=/root/tgmsa
 BOXES=395
+OPENHOUR
+CLOSEHOUR
+OPENHOURSEC
+CLOSEHOURSEC
+
 #DATE=`date +"%Y-%m-%d"`
 DATE="2017-04-01"
 
@@ -13,4 +18,5 @@ for i in $(seq 393 395); do
 	bash $TGMSAHOME/dbData.sh $i | grep -v czas| grep $DATE | awk 'NR==1; END{print}'| sed "s/$DATE//g"|cut -d " " -f4|tr '\n' ',' >> $TGMSAHOME/$DATE/report-$DATE.csv
 	BOXNAME=`mysql accoDb -e "select nazwaUzytkownika from Zdarzenie where (idUzytkownik = $i)"|sort -u|grep -v nazwaUzytkownika`
 	echo $BOXNAME >> $TGMSAHOME/$DATE/report-$DATE.csv
+	echo $TGMSAHOME/$DATE/report-$DATE.csv
 done
