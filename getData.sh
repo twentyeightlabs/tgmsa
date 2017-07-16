ac02a9fc5ba7cea2f3c6e2f5c8ff77836737345e
twentyeightlabs <artur@twentyeightlabs.com>
2017-07-16 15:19:59 +0200

    change separator to comma

--- getData.sh
+++ getData.sh
@@ -101,7 +101,7 @@ for i in $(seq 406 415); do
 	echo "w godzinach ${TIMEOPENMONTH}"
 	echo "w godzinach2 ${TIMEOPENH} $(($TIMEOPENMONTH / 60)) $(($TIMEOPENMONTH % 60))"
 	echo "$(($TIMEOPENMONTH / 60)).$(($TIMEOPENMONTH % 60))"  > $TGMSAHOME/reports/$MDATE/BOX-$i/BOX-$i-H.csv
-	echo "BOX-$i;$(($TIMEOPENMONTH / 60)).$(($TIMEOPENMONTH % 60))"  > $TGMSAHOME/reports/$MDATE/$MDATE-BOX-$i.csv
+	echo "BOX-$i,$(($TIMEOPENMONTH / 60)).$(($TIMEOPENMONTH % 60))"  > $TGMSAHOME/reports/$MDATE/$MDATE-BOX-$i.csv
 	cp html-template/report-template-monthly.html $REPORTSHOME/$MDATE/BOX-$i/BOX-$i-H.html
 	sed -i "s/INSERT-MONTHLY-DATE/$MDATE/g" $REPORTSHOME/$MDATE/BOX-$i/BOX-$i-H.html
 	sed -i "s/INSERT-BOX-NUMBER/$i/g" $REPORTSHOME/$MDATE/BOX-$i/BOX-$i-H.html
