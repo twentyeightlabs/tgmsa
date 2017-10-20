92ffa66b775f787680c8f41f529f45cb2c603dab
Debian <admin@ip-172-31-29-212.eu-central-1.compute.internal>
2017-10-20 15:48:08 +0000

    sbstitute end and start

--- tgmsa.py
+++ tgmsa.py
@@ -6,14 +6,14 @@ import time
 import datetime
 
 
-def connectToDb():
+def connectToDb(box_id):
     try:
         connection = MySQLdb.connect(host='127.0.0.1', user='tgmsa', passwd='password', db='accoDb')
         cursor=connection.cursor()
 
         query=("SELECT czasZdarzenia, idStrefaWejsciowa, nazwaStrefyWejsciowej, nazwaStrefyWyjsciowej, idStrefaWyjsciowa, idKontroler, szczegoly, nazwaKontrolera FROM Zdarzenie a WHERE (idUzytkownik = %s) AND (kodZdarzenia = 263) AND (czasZdarzenia >= %s) AND (czasZdarzenia < %s) AND (idStrefaWejsciowa = '1' OR idStrefaWyjsciowa = '1') AND (SUBSTR(CONV(HEX(szczegoly),16,2), -12, 1) !=1) ORDER BY czasZdarzenia ASC;")
         
-        box_id='414'
+        #box_id='414'
         day_start = datetime.date(2017, 05, 10)
         day_end = datetime.date(2017, 05, 11)
 
@@ -26,10 +26,18 @@ def connectToDb():
     except Exception as e:
         print e
 
+
+    if not data:
+        print('Structure is empty.')
+        data=('0,0')
+
     start=data[0][0]
     end=data[-1][0]
-    #print('otwarto o:', start)
-    #print('zamknieto o: ', end)
+    print(start)
+    print(end)
+
+    worked=end-start
+    print(worked)
 
 #End connectToDb
 
@@ -38,17 +46,19 @@ def getDate():
     print time.asctime(time.localtime(time.time()))
     curr_date='2017-05-11'
     print(curr_date)
+
 #End getTime
 
-def getBox(box_number):
-    print('numer boxu')
-    
-
-#End getBox
 
 def main():
-    #getDate()
-    connectToDb()
+    getDate()
+    tgmsa_home='/home/admin'
+    report_dir=tgmsa_home+'/reports'
+    print(tgmsa_home)
+    print(report_dir)
+    for box in range(414, 415):
+        connectToDb(box)
+        print(box)
 #End main
 
 
