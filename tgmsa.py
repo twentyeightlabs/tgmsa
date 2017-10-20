cca287ad3928b12566c5e3b90a641de3a94e4044
Debian <admin@ip-172-31-29-212.eu-central-1.compute.internal>
2017-10-20 10:40:08 +0000

    initial commit

--- tgmsa.py
+++ tgmsa.py
@@ -3,30 +3,49 @@
 import MySQLdb
 import sys
 import time
+import datetime
+
 
 def connectToDb():
+    user_id='414'
     try:
         connection = MySQLdb.connect(host='127.0.0.1', user='tgmsa', passwd='password', db='accoDb')
         cursor=connection.cursor()
-        cursor.execute("select * from Uzytkownik")
+
+        query=("SELECT czasZdarzenia, idStrefaWejsciowa, nazwaStrefyWejsciowej, nazwaStrefyWyjsciowej, idStrefaWyjsciowa, idKontroler, szczegoly, nazwaKontrolera FROM Zdarzenie a WHERE (idUzytkownik = 414) AND (kodZdarzenia = 263) AND (czasZdarzenia >= %s) AND (czasZdarzenia < %s) AND (idStrefaWejsciowa = '1' OR idStrefaWyjsciowa = '1') AND (SUBSTR(CONV(HEX(szczegoly),16,2), -12, 1) !=1) ORDER BY czasZdarzenia ASC;")
+        
+        hire_start = datetime.date(2017, 05, 10)
+        hire_end = datetime.date(2017, 05, 11)
+
+        cursor.execute(query, (hire_start, hire_end))
         data = cursor.fetchall()
+
         connection.close()
+        cursor.close()
+
     except Exception as e:
         print e
 
     for row in data:
-        print row[1], row[11]
+        print(data)
 #End connectToDb
 
 
-def getTime():
+def getDate():
     print time.asctime(time.localtime(time.time()))
+    curr_date='2017-05-11'
+    print(curr_date)
 #End getTime
 
+def getBox(box_number):
+    print('numer boxu')
+    
+
+#End getBox
 
 def main():
+    getDate()
     connectToDb()
-    getTime()
 #End main
 
 
