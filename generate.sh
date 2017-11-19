c12c220ea165c72dcebd40ab6d09ffee32b314b9
twentyeightlabs <artur@twentyeightlabs.com>
2017-11-19 22:15:29 +0100

    06-11-2017 report

--- generate.sh
+++ generate.sh
@@ -5,9 +5,9 @@ TGMSAHOME='/Users/stachu/work/tgmsa-reports/'
 REPORTSHOME=$TGMSAHOME/reports
 
 #DATE=`date +"%Y-%m-%d"`
-DATE="2017-11-04"
+DATE="2017-11-06"
 #REPORTDATE=`date +"%d-%m-%Y"`
-REPORTDATE="04-11-2017"
+REPORTDATE="06-11-2017"
 #MDATE=`date +"%Y-%m"`
 MDATE="2017-11"
 #MREPORTDATE=`date +"%m-%Y"`
