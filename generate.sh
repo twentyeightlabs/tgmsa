3453b1f65df3b32022a34e057a245cc50ad03e69
michal <michal@debian>
2018-05-26 07:45:09 +0200

    generate report for 26-05-2018

--- generate.sh
+++ generate.sh
@@ -5,10 +5,10 @@ TGMSAHOME='/home/michal/tgmsa'
 REPORTSHOME=$TGMSAHOME/reports
 
 #DATE=`date +"%Y-%m-%d"`
-DATE="2018-05-21"
+DATE="2018-05-19"
 
 #REPORTDATE=`date +"%d-%m-%Y"`
-REPORTDATE="21-05-2018"
+REPORTDATE="19-05-2018"
 
 #MDATE=`date +"%Y-%m"`
 MDATE="2018-05"
